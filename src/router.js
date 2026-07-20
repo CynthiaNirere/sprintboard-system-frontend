cfa40b1ea0e8193aa4a92b75ec645c4f306f7f9e
CynthiaNirere <cynthia.nirere@eagles.oc.edu>
2026-07-20 17:46:35 -0500

    added sprint view

--- src/router.js
+++ src/router.js
@@ -37,8 +37,8 @@ const router = createRouter({
       path: "/admin",
       name: "adminLayout",
       component: () => import("./components/AdminLayout.vue"),
-      redirect: { name: "adminOverview" },
-      beforeEnter: (to, from, next) => {
+     redirect: { name: "overview" },
+     beforeEnter: (to, from, next) => {
         const user = JSON.parse(localStorage.getItem("user") || "{}");
         if (user.globalRole === "ADMIN") next();
         else next({ name: "userOverview" });

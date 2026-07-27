5bdc56eb453853830229c88800098258007d3581
Justin Walraven <justin.p.walraven@gmail.com>
2026-07-26 20:16:03 -0500

    Sprint planning admin page

--- src/components/UserSidebar.vue
+++ src/components/UserSidebar.vue
@@ -88,7 +88,7 @@ function formatRole(role) {
           </div>
         </v-list-item>
 
-          <v-list-item :to="{ name: 'userBacklog' }" class="mx-3 rounded-lg" active-class="active-tab">
+          <v-list-item :to="{ name: 'sprints' }" :to="{ name: 'userBacklog' }" class="mx-3 rounded-lg" active-class="active-tab">
           <div class="d-flex ga-3 align-center">
             <v-icon>mdi-rocket-launch-outline</v-icon>
             <span> Backlog </span>

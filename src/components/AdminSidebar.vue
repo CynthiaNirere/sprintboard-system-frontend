5cb9d6ce16e0978ff3ee5283a679e809ffc1b4a3
CynthiaNirere <cynthia.nirere@eagles.oc.edu>
2026-07-27 21:06:04 -0500

    updated admin backlog clipboard

--- src/components/AdminSidebar.vue
+++ src/components/AdminSidebar.vue
@@ -94,8 +94,8 @@ function formatRole(role) {
           </div>
         </v-list-item>
             <v-list-item :to="{ name: 'backlog' }" class="mx-3 rounded-lg" active-class="active-tab">
-            <div class="d-flex ga-3 align-center">
-              <v-icon>mdi-rocket-launch-outline</v-icon>
+          <div class="d-flex ga-3 align-center">
+            <v-icon>mdi-clipboard-text-outline</v-icon>
               <span>Backlog</span>
             </div>
           </v-list-item>

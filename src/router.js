96bb1fe76ceba2383412586a17cd6a547a8d1d51
Daniel T. Fijalka <daniel.fijalka@hobbylobby.com>
2026-08-04 17:05:46 -0500

    feat: initial setup

--- src/router.js
+++ src/router.js
@@ -47,6 +47,11 @@ const router = createRouter({
           component: () => import("./views/TeamManagement.vue"),
         },
         {
+          path: "board-statuses",
+          name: "boardStatuses",
+          component: () => import("./views/BoardStatuses.vue"),
+        },
+        {
           path: "projects",
           name: "adminProjects",
           component: () => import("./views/ProjectsView.vue"),
@@ -127,7 +132,12 @@ const router = createRouter({
           path: "team-management",
           name: "projectAdminTeamManagement",
           component: () => import("./views/TeamManagement.vue"),
-        }
+        },
+        {
+          path: "board-statuses",
+          name: "projectAdminBoardStatuses",
+          component: () => import("./views/BoardStatuses.vue"),
+        },
       ],
     },
 ],

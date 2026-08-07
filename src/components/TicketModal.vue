745a84ef8fec4ed472c0c12f55939d4fdf0a52ab
Daniel T. Fijalka <daniel.fijalka@hobbylobby.com>
2026-08-07 17:22:34 -0500

    feat: add assignee and tester selects in modal, adjust styling within storyboard view

--- src/components/TicketModal.vue
+++ src/components/TicketModal.vue
@@ -1,16 +1,16 @@
 <script setup>
-import { defineProps, onMounted, defineEmits, ref, toRaw} from "vue";
-import TicketServices from "../services/TicketServices";
+import { defineProps, defineEmits, ref } from "vue";
 import {onClickOutside} from '@vueuse/core'
 import TicketModalHeader from "./TicketModalHeader.vue";
 import TicketModalNav from "./TicketModalNav.vue";
-import TicketModalFooter from "./TicketModalFooter.vue"
 
 const props = defineProps({
   isOpen: Boolean,
   ticket: Object,
   addTicket: Boolean,
   snackbar: Object,
+  boardStatuses: Object,
+  projectMembers: Object
 });
 
 const emit = defineEmits(["modal-close", "ticket-count-changed"]);
@@ -19,82 +19,30 @@ const target = ref(null);
 onClickOutside(target, ()=>emit('modal-close'),{
   ignore: ['.v-overlay-container']
 });
-
-async function submit(){
-  if(props.addTicket){
-    await TicketServices.addTicket(props.ticket)
-      .then((response) => {
-        props.snackbar.value = true;
-        props.snackbar.color = "green";
-        props.snackbar.text = `${props.ticket.status} updated successfully!`;
-        emit('modal-close');
-
-      })
-      .catch((error) => {
-        console.log(error);
-        
-        props.snackbar.value = true;
-        props.snackbar.color = "error";
-        props.snackbar.text = error.response?.data?.message || "Error creating Ticket";
-      });
-      emit('ticket-count-changed');
-  }else{
-    await TicketServices.updateTicket(props.ticket.id, props.ticket)
-    .then((response) => {
-      props.snackbar.value = true;
-      props.snackbar.color = "green";
-      props.snackbar.text = `${props.ticket.status} updated successfully!`;
-      emit('modal-close');
-
-    })
-    .catch((error) => {
-      console.log(error);
-      
-      props.snackbar.value = true;
-      props.snackbar.color = "error";
-      props.snackbar.text = error.response?.data?.message || "Error updating ticket";
-    });
-  }
-}
-
-async function del(){
-  await TicketServices.deleteTicket(props.ticket.id)
-    .then(() => {
-      props.snackbar.value = true;
-      props.snackbar.color = "green";
-      props.snackbar.text = `Ticket deleted successfully!`;
-    })
-    .catch((error) => {
-      console.log(error);
-      props.snackbar.value = true;
-      props.snackbar.color = "error";
-      props.snackbar.text = error.response.data.message;
-    });
-  emit('ticket-count-changed');
-  emit('modal-close');
-
-}
 </script>
 
 <template>
   <div v-if="isOpen" class="modal-mask">
-    <div class="modal-wrapper">
-      <div class="modal-container" ref="target">
-        <TicketModalHeader 
-          class="px-4 pt-4"
-          :activeTicket="props.ticket"
-          @modal-close="emit('modal-close')"
-        />
-        <TicketModalNav 
-          class="px-4"
-          :activeTicket="props.ticket"
-        />
-        <TicketModalFooter 
-          class="pb-4"
-        
-        />
-      </div>
-    </div>
+    <v-container fluid class="fill-height">
+      <v-row justify="center">
+        <v-card class="modal-container" ref="target">
+          <TicketModalHeader 
+            :activeTicket="props.ticket"
+            :addTicket="props.addTicket"
+            @modal-close="emit('modal-close')"
+          />
+          <TicketModalNav 
+            :activeTicket="props.ticket"
+            :addTicket="props.addTicket"
+            :snackbar="props.snackbar"
+            :boardStatuses="props.boardStatuses"
+            :projectMembers="props.projectMembers"
+            @modal-close="emit('modal-close')"
+            @ticket-count-changed="emit('ticket-count-changed')"
+          />
+        </v-card>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
@@ -110,10 +58,10 @@ async function del(){
 }
 .modal-container {
   width: 50%;
-  margin: 150px auto;
+  margin: 0 auto;
   /* padding: 20px 30px; */
   background-color: #fff;
-  border-radius: 2px;
+  border-radius: 20px;
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
 }
 </style>
